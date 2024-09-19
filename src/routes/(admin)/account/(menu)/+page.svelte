8577b27c225ef31e614321ca027bb8cc14806343
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-09-18 22:28:44 -0500

    Add deeplink toggle warning message

--- src/routes/(admin)/account/(menu)/+page.svelte
+++ src/routes/(admin)/account/(menu)/+page.svelte
@@ -322,9 +322,23 @@
           name="deeplinkToggle"
           id="deeplink_toggle"
         />
-        <p class="text-xs mt-2 text-gray-600">
-          Warning: This feature is experimental. If the deeplink service is not
-          working correctly, it will default to normal links.
+        <p class="text-md mt-2 text-gray-600 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 shrink-0 text-yellow-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          This feature is experimental. If the deeplink service is not working correctly,
+          it will default to normal links.
         </p>
       </div>
     </div>
